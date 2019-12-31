fced3ed6e26b7036b5cb2c6d31cd0667ef3c41bb
tomergev <tomergev21@gmail.com>
2019-12-30 17:26:23 -0800

    Worked on the logic to create a quiz

--- src/components/GeoQuiz.vue
+++ src/components/GeoQuiz.vue
@@ -7,25 +7,77 @@ export default {
   name: "GeoQuiz",
   data() {
     return {
-      europeanCountries: []
+      europeanCountriesArr: [],
+      europeanCountriesObj: {},
+      quizQuestions: []
     };
   },
   methods: {
-    async fetch() {
+    async fetchEuropeanCountries() {
       try {
         const res = await fetch(
           "https://restcountries.eu/rest/v2/region/europe"
         );
-        const europeanCountries = await res.json();
-        console.log(europeanCountries);
-        this.europeanCountries = europeanCountries;
+        const europeanCountriesArr = await res.json();
+
+        const europeanCountriesObj = europeanCountriesArr.reduce(
+          (acc, country) => {
+            acc[country.name] = country;
+            return acc;
+          },
+          {}
+        );
+
+        console.log({ europeanCountriesObj, europeanCountriesArr });
+        this.europeanCountriesArr = europeanCountriesArr;
+        this.europeanCountriesObj = europeanCountriesObj;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    createQuizQuestions() {
+      try {
+        const generateRandomNumber = (min = 0, max = 52) => {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
+        const quizQuestions = this.europeanCountriesArr.map(country => {
+          const possibleChoices = [country.name];
+
+          const choices = [country, {}, {}, {}].map((val, ind) => {
+            if (!ind) return val;
+
+            const selectCountry = () => {
+              const randomIndex = generateRandomNumber();
+              const randomCountry = this.europeanCountriesArr[randomIndex];
+
+              if (
+                possibleChoices.some(
+                  countryName => randomCountry.name === countryName
+                )
+              ) {
+                return selectCountry();
+              } else {
+                possibleChoices.push(randomCountry.name);
+                return randomCountry;
+              }
+            };
+
+            return selectCountry();
+          });
+
+          console.log(choices);
+        });
       } catch (err) {
         console.log(err);
       }
     }
   },
-  created() {
-    this.fetch();
+  async created() {
+    await this.fetchEuropeanCountries();
+    await this.createQuizQuestions();
   }
 };
 </script>
